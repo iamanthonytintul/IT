c93347bd7704fa41975c416c6c8c510efe9d9def
Anthony Tintul <antontintul@gmail.com>
2020-03-25 10:08:11 +0300

    Настройка CI

--- libs/dynamic_employee_DB.c
+++ libs/dynamic_employee_DB.c
@@ -35,70 +35,7 @@ boolean init_pthreads_data(subarray **pthreads_data, FILE *output_stream, employ
     return TRUE;
 }
 
-boolean print_the_most_aged_employees_in_each_position_dyn(FILE *output_stream, employee_info **emp, int capacity_of_emp) {
 
-    int amount_of_positions = 0;
-    char **unique_positions = find_unique_positions(emp, capacity_of_emp, &amount_of_positions);
-
-    if (!unique_positions) {
-        return FALSE;
-    }
-
-    int number_of_threads = get_nprocs();
-    pthread_t *pthreads = (pthread_t *) malloc(number_of_threads * sizeof(pthread_t));
-    subarray **pthreads_data = (subarray **) malloc(number_of_threads * sizeof(subarray *));
-
-    if (!pthreads || !pthreads_data) {
-        return FALSE;
-    }
-
-    if (!init_pthreads_data(pthreads_data, output_stream, emp, capacity_of_emp,
-                            unique_positions, number_of_threads, amount_of_positions)) {
-        return FALSE;
-    }
-
-    int err_flag = FALSE;
-    mark_all_as_unchecked(emp, capacity_of_emp);
-    for (int i = 0; i < number_of_threads && !err_flag; i++) {
-        err_flag = pthread_create(&(pthreads[i]), NULL, &get_the_youngest_in_positions_dynamic, pthreads_data[i]);
-        cpu_set_t cpu_set;
-        CPU_ZERO(&cpu_set);
-        CPU_SET(i, &cpu_set);
-        pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
-    }
-
-    for (int i = 0; i < number_of_threads; i++) {
-        pthread_join(pthreads[i], NULL);
-    }
-
-    print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "YOUNG");
-
-    mark_all_as_unchecked(emp, capacity_of_emp);
-    for (int i = 0; i < number_of_threads && !err_flag; i++) {
-        err_flag = pthread_create(&(pthreads[i]), NULL, &get_the_oldest_in_positions_dynamic, pthreads_data[i]);
-        cpu_set_t cpu_set;
-        CPU_ZERO(&cpu_set);
-        CPU_SET(i, &cpu_set);
-        pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
-    }
-
-    for (int i = 0; i < number_of_threads; i++) {
-        pthread_join(pthreads[i], NULL);
-    }
-
-    print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "OLD");
-
-    free(pthreads);
-    free_pthread_data(pthreads_data, number_of_threads);
-    free(unique_positions);
-
-
-    if (err_flag) {
-        return FALSE;
-    }
-
-    return TRUE;
-}
 
 void *get_the_youngest_in_positions_dynamic(void *data) {
 
@@ -172,6 +109,70 @@ boolean free_pthread_data(subarray **pthread_data, int number_of_threads) {
         }
     }
     free(pthread_data);
+    return TRUE;
+}
+boolean print_the_most_aged_employees_in_each_position_dyn(FILE *output_stream, employee_info **emp, int capacity_of_emp) {
+
+    int amount_of_positions = 0;
+    char **unique_positions = find_unique_positions(emp, capacity_of_emp, &amount_of_positions);
+
+    if (!unique_positions) {
+        return FALSE;
+    }
+
+    int number_of_threads = get_nprocs();
+    pthread_t *pthreads = (pthread_t *) malloc(number_of_threads * sizeof(pthread_t));
+    subarray **pthreads_data = (subarray **) malloc(number_of_threads * sizeof(subarray *));
+
+    if (!pthreads || !pthreads_data) {
+        return FALSE;
+    }
+
+    if (!init_pthreads_data(pthreads_data, output_stream, emp, capacity_of_emp,
+                            unique_positions, number_of_threads, amount_of_positions)) {
+        return FALSE;
+    }
+
+    int err_flag = FALSE;
+    mark_all_as_unchecked(emp, capacity_of_emp);
+    for (int i = 0; i < number_of_threads && !err_flag; i++) {
+        err_flag = pthread_create(&(pthreads[i]), NULL, &get_the_youngest_in_positions_dynamic, pthreads_data[i]);
+        cpu_set_t cpu_set;
+        CPU_ZERO(&cpu_set);
+        CPU_SET(i, &cpu_set);
+        pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
+    }
+
+    for (int i = 0; i < number_of_threads; i++) {
+        pthread_join(pthreads[i], NULL);
+    }
+
+    print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "YOUNG");
+
+    mark_all_as_unchecked(emp, capacity_of_emp);
+    for (int i = 0; i < number_of_threads && !err_flag; i++) {
+        err_flag = pthread_create(&(pthreads[i]), NULL, &get_the_oldest_in_positions_dynamic, pthreads_data[i]);
+        cpu_set_t cpu_set;
+        CPU_ZERO(&cpu_set);
+        CPU_SET(i, &cpu_set);
+        pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
+    }
+
+    for (int i = 0; i < number_of_threads; i++) {
+        pthread_join(pthreads[i], NULL);
+    }
+
+    print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "OLD");
+
+    free(pthreads);
+    free_pthread_data(pthreads_data, number_of_threads);
+    free(unique_positions);
+
+
+    if (err_flag) {
+        return FALSE;
+    }
+
     return TRUE;
 }
 
