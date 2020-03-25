a5b576e479c1ad1b165c6715fe1657451c2e9e1f
Anthony Tintul <antontintul@gmail.com>
2020-03-25 10:03:01 +0300

    Настройка CI

--- libs/dynamic_employee_DB.c
+++ libs/dynamic_employee_DB.c
@@ -57,36 +57,36 @@ boolean print_the_most_aged_employees_in_each_position_dyn(FILE *output_stream, 
         return FALSE;
     }
 
-   int err_flag = FALSE;
-     //mark_all_as_unchecked(emp, capacity_of_emp);
+    int err_flag = FALSE;
+    mark_all_as_unchecked(emp, capacity_of_emp);
     for (int i = 0; i < number_of_threads && !err_flag; i++) {
-        err_flag = pthread_create(&(pthreads[i]), NULL, get_the_youngest_in_positions_dynamic, pthreads_data[i]);
-        //cpu_set_t cpu_set;
-        //CPU_ZERO(&cpu_set);
-        //CPU_SET(i, &cpu_set);
-        //pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
+        err_flag = pthread_create(&(pthreads[i]), NULL, &get_the_youngest_in_positions_dynamic, pthreads_data[i]);
+        cpu_set_t cpu_set;
+        CPU_ZERO(&cpu_set);
+        CPU_SET(i, &cpu_set);
+        pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
     }
 
     for (int i = 0; i < number_of_threads; i++) {
         pthread_join(pthreads[i], NULL);
     }
 
-    //print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "YOUNG");
+    print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "YOUNG");
 
-    //mark_all_as_unchecked(emp, capacity_of_emp);
+    mark_all_as_unchecked(emp, capacity_of_emp);
     for (int i = 0; i < number_of_threads && !err_flag; i++) {
-        err_flag = pthread_create(&(pthreads[i]), NULL, get_the_oldest_in_positions_dynamic, pthreads_data[i]);
-        //cpu_set_t cpu_set;
-        //CPU_ZERO(&cpu_set);
-        //CPU_SET(i, &cpu_set);
-        //pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
+        err_flag = pthread_create(&(pthreads[i]), NULL, &get_the_oldest_in_positions_dynamic, pthreads_data[i]);
+        cpu_set_t cpu_set;
+        CPU_ZERO(&cpu_set);
+        CPU_SET(i, &cpu_set);
+        pthread_setaffinity_np(pthreads[i], sizeof(cpu_set), &cpu_set);
     }
 
     for (int i = 0; i < number_of_threads; i++) {
         pthread_join(pthreads[i], NULL);
     }
 
-    //print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "OLD");
+    print_employees_in_age(output_stream, emp, capacity_of_emp, unique_positions, amount_of_positions, "OLD");
 
     free(pthreads);
     free_pthread_data(pthreads_data, number_of_threads);
