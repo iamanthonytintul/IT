fb62496e9462b3746342ecdca53ef39daab13bb1
Anthony Tintul <antontintul@gmail.com>
2020-03-25 08:26:22 +0300

    Настройка CI

--- test/functional_tests/functional.cpp
+++ test/functional_tests/functional.cpp
@@ -2,11 +2,12 @@
 
 extern "C" {
 
+#include <dlfcn.h>
 #include "static_employee_DB.h"
 #include "count_time.h"
 
-#include <dlfcn.h>
 }
+
 class Functional {
 
 public:
@@ -18,25 +19,22 @@ public:
 
     boolean (*sort_by_surname)(employee_info **emp, int capacity_of_emp);
 
-    boolean (*print_the_most_aged_employees_in_each_position_dyn)(FILE *output_stream, employee_info **emp, int capacity_of_emp);
+    boolean (*print_the_most_aged_employees_in_each_position_dyn)(FILE *input_stream, employee_info **emp,
+                                                                  int capacity_of_emp);
 
     boolean (*free_employees)(employee_info **emp, int capacity_of_emp);
 };
 
 class Functional_test : public ::testing::Test {
 protected:
-    void SetUp() override {
+    void SetUp() {
         dyn_lib.library = dlopen(PATH_TO_DYNAMIC_LIB, RTLD_LAZY);
-        *(void **) (&dyn_lib.read_number) = dlsym(dyn_lib.library, "read_number");
-        *(void **) (&dyn_lib.read_employees) = dlsym(dyn_lib.library, "read_employees");
-        *(void **) (&dyn_lib.sort_by_surname) = dlsym(dyn_lib.library, "sort_by_surname");
         *(void **) (&dyn_lib.print_the_most_aged_employees_in_each_position_dyn) =
                 dlsym(dyn_lib.library, "print_the_most_aged_employees_in_each_position_dyn");
-        *(void **) (&dyn_lib.free_employees) = dlsym(dyn_lib.library, "free_employees");
 
     }
 
-    void TearDown() override {
+    void TearDown() {
         dlclose(dyn_lib.library);
     }
 
@@ -54,25 +52,27 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
     EXPECT_TRUE(output_static_stream != NULL);
     EXPECT_TRUE(output_dynamic_stream != NULL);
 
-    int capacity_of_employees = (int)dyn_lib.read_number(input_stream);
-    /*if (capacity_of_employees != FAILURE) {
-        employee_info **employees = dyn_lib->read_employees(input_stream, capacity_of_employees);
-
-        if (employees) {
-            dyn_lib->sort_by_surname(employees, capacity_of_employees);
-            dyn_lib->print_the_most_aged_employees_in_each_position_dyn(output_dynamic_stream, employees, capacity_of_employees);
-        }
-        dyn_lib->free_employees(employees, capacity_of_employees);
-    }
-
-    rewind(input_stream);
     int capacity_of_employees = read_number(input_stream);
     if (capacity_of_employees != FAILURE) {
         employee_info **employees = read_employees(input_stream, capacity_of_employees);
 
         if (employees) {
             sort_by_surname(employees, capacity_of_employees);
-            print_the_most_aged_employees_in_each_position_static(output_static_stream, employees, capacity_of_employees);
+            dyn_lib.print_the_most_aged_employees_in_each_position_dyn(output_dynamic_stream, employees,
+                                                                       capacity_of_employees);
+        }
+        free_employees(employees, capacity_of_employees);
+    }
+
+    rewind(input_stream);
+    capacity_of_employees = read_number(input_stream);
+    if (capacity_of_employees != FAILURE) {
+        employee_info **employees = read_employees(input_stream, capacity_of_employees);
+
+        if (employees) {
+            sort_by_surname(employees, capacity_of_employees);
+            print_the_most_aged_employees_in_each_position_static(output_static_stream, employees,
+                                                                  capacity_of_employees);
         }
         free_employees(employees, capacity_of_employees);
     }
@@ -86,15 +86,14 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
         char_output_static = fgetc(output_static_stream);
         char_output_dynamic = fgetc(output_dynamic_stream);
         EXPECT_EQ(char_output_static, char_output_dynamic);
-    }*/
+    }
 
-    EXPECT_EQ(capacity_of_employees, 10000);
     fclose(input_stream);
     fclose(output_dynamic_stream);
     fclose(output_static_stream);
 }
 
-/*TEST_F(Functional_test, HandlesSecondRegularCase) {
+TEST_F(Functional_test, HandlesSecondRegularCase) {
 
     FILE *input_stream = fopen(PATH_FUNCTIONAL_INPUT_SECOND_REGULAR_CASE, "r");
     FILE *output_static_stream = fopen(PATH_FUNCTIONAL_OUTPUT_STATIC_LIB, "w+");
@@ -118,15 +117,16 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
 
     rewind(input_stream);
 
-    capacity_of_employees = dyn_lib->read_number(input_stream);
+    capacity_of_employees = read_number(input_stream);
     if (capacity_of_employees != FAILURE) {
-        employee_info **employees = dyn_lib->read_employees(input_stream, capacity_of_employees);
+        employee_info **employees = read_employees(input_stream, capacity_of_employees);
 
         if (employees) {
-            dyn_lib->sort_by_surname(employees, capacity_of_employees);
-          //  dyn_lib->print_the_most_aged_employees_in_each_position_dyn(output_dynamic_stream, employees, capacity_of_employees);
+            sort_by_surname(employees, capacity_of_employees);
+            dyn_lib.print_the_most_aged_employees_in_each_position_dyn(output_dynamic_stream, employees,
+                                                                       capacity_of_employees);
         }
-        dyn_lib->free_employees(employees, capacity_of_employees);
+        free_employees(employees, capacity_of_employees);
     }
 
     rewind(output_static_stream);
@@ -134,7 +134,7 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
 
     int char_output_static = 0;
     int char_output_dynamic = 0;
-    while (char_output_static != EOF || char_output_dynamic != EOF){
+    while (char_output_static != EOF || char_output_dynamic != EOF) {
         char_output_static = fgetc(output_static_stream);
         char_output_dynamic = fgetc(output_dynamic_stream);
         EXPECT_EQ(char_output_static, char_output_dynamic);
@@ -144,5 +144,4 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
     fclose(output_dynamic_stream);
     fclose(output_static_stream);
 }
-*/
 
