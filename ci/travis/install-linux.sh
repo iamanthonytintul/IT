db3a4bee8fc961a03a8531bb55179898a0b1f94b
Anthony Tintul <antontintul@gmail.com>
2020-03-25 03:58:05 +0300

    Настройка CI

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,6 +1,8 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 
+sudo apt-get install gcc-7
+sudo apt-get install g++-7
 sudo apt install valgrind
 sudo apt install cppcheck
 pip install --user cpp-coveralls
