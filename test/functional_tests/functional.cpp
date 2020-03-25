3aae04abd71b6430f89c5b00bd0832e3983cd49b
Anthony Tintul <antontintul@gmail.com>
2020-03-25 05:26:28 +0300

    Настройка CI

--- test/functional_tests/functional.cpp
+++ test/functional_tests/functional.cpp
@@ -57,7 +57,20 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
     EXPECT_TRUE(output_static_stream != NULL);
     EXPECT_TRUE(output_dynamic_stream != NULL);
 
-    int capacity_of_employees = read_number(input_stream);
+    int capacity_of_employees = dyn_lib->read_number(input_stream);
+    if (capacity_of_employees != FAILURE) {
+        employee_info **employees = dyn_lib->read_employees(input_stream, capacity_of_employees);
+
+        if (employees) {
+            dyn_lib->sort_by_surname(employees, capacity_of_employees);
+            dyn_lib->print_the_most_aged_employees_in_each_position(output_dynamic_stream, employees,
+                                                                    capacity_of_employees);
+        }
+        dyn_lib->free_employees(employees, capacity_of_employees);
+    }
+
+    rewind(input_stream);
+    capacity_of_employees = read_number(input_stream);
     if (capacity_of_employees != FAILURE) {
         employee_info **employees = read_employees(input_stream, capacity_of_employees);
 
@@ -69,35 +82,21 @@ TEST_F(Functional_test, HandlesFirstRegularCase) {
         free_employees(employees, capacity_of_employees);
     }
 
-    fclose(input_stream);
-    fclose(output_dynamic_stream);
-    fclose(output_static_stream);
-}
+    rewind(output_static_stream);
+    rewind(output_dynamic_stream);
 
-TEST_F(Functional_test, HandlesSecondRegularCase) {
-
-    FILE *input_stream = fopen(PATH_FUNCTIONAL_INPUT_SECOND_REGULAR_CASE, "r");
-    FILE *output_static_stream = fopen(PATH_FUNCTIONAL_OUTPUT_STATIC_LIB, "w+");
-    FILE *output_dynamic_stream = fopen(PATH_FUNCTIONAL_OUTPUT_DYNAMIC_LIB, "w+");
-
-    EXPECT_TRUE(input_stream != NULL);
-    EXPECT_TRUE(output_static_stream != NULL);
-    EXPECT_TRUE(output_dynamic_stream != NULL);
-
-    int capacity_of_employees = read_number(input_stream);
-    if (capacity_of_employees != FAILURE) {
-        employee_info **employees = read_employees(input_stream, capacity_of_employees);
-
-        if (employees) {
-            sort_by_surname(employees, capacity_of_employees);
-            print_the_most_aged_employees_in_each_position_static(output_static_stream, employees,
-                                                                  capacity_of_employees);
-        }
-        free_employees(employees, capacity_of_employees);
+    int char_output_static = 0;
+    int char_output_dynamic = 0;
+    while (char_output_static != EOF || char_output_dynamic != EOF) {
+        char_output_static = fgetc(output_static_stream);
+        char_output_dynamic = fgetc(output_dynamic_stream);
+        EXPECT_EQ(char_output_static, char_output_dynamic);
     }
 
-
     fclose(input_stream);
     fclose(output_dynamic_stream);
     fclose(output_static_stream);
 }
+
+
+
